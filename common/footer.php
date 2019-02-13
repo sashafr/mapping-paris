121b5c5362c57101d6c250a6981f8d951bdf8acc
najay <najayg@sas.upenn.edu>
2019-02-13 17:03:28 +0000

    refactoring common attributes

--- common/footer.php
+++ common/footer.php
@@ -1,55 +1,23 @@
-<?php
-
-/**
- * @package     omeka
- * @subpackage  neatline-NeatLight
- * @copyright   2014 Rector and Board of Visitors, University of Virginia
- * @license     http://www.apache.org/licenses/LICENSE-2.0.html
- */
-
-?>
-</div>
-
-<footer id="footer">
-  <div class="inner">
-
-    <h3>What are you looking for?</h3>
-    <form action="#" method="post">
-      <div id="search-container" role="search">
-          <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
-          <?php echo search_form(array('show_advanced' => true)); ?>
-          <?php else: ?>
-          <?Php echo search_form(); ?>
-          <?php endif; ?>
-      </div>
-    <div class="copyright">
-      &copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
-    </div><!--end wrap-->
-
-    <!-- Footer -->
+<!-- Footer -->
       <footer id="footer">
-        <ul class="icons">
-          <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-          <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-          <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-          <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-          <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-          <li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-        </ul>
-        <ul class="copyright">
-          <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-        </ul>
-      </footer>
+        <div id="footer-text">
+              <?php echo get_theme_option('Footer Text'); ?>
+              <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+                  <p><?php echo $copyright; ?></p>
+              <?php endif; ?>
+              <p><?php echo __('A <a target="_blank" href="https://guides.library.upenn.edu/omeka">Penn Libraries Omeka</a> Site'); ?></p>
+          </div>
 
-<script type="text/javascript">
-jQuery(document).ready(function () {
-    Omeka.showAdvancedForm();
-    Omeka.skipNav();
-    Omeka.megaMenu("#top-nav");
-    Seasons.mobileSelectNav();
-});
-</script>
-
+          <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+        </footer>
+              
+  <script type="text/javascript">
+  jQuery(document).ready(function () {
+      Omeka.showAdvancedForm();
+      Omeka.skipNav();
+      Omeka.megaMenu("#top-nav");
+      Seasons.mobileSelectNav();
+  });
+  </script>
 </body>
-
 </html>
--- common/header.php
+++ common/header.php
@@ -1,73 +1,51 @@
 <!DOCTYPE HTML>
 <html lang="<?php echo get_html_lang(); ?>">
-	<head>
-		<meta charset="utf-8" />
+<head>
+    <meta charset="utf-8" />
     <?php if ( $description = option('description')): ?>
         <meta name="description" content="<?php echo $description; ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php endif; ?>
-		<link rel="stylesheet" href="themes/mapping-paris/css/neatlite-najay.css" />
-	</head>
-	<body class="landing is-preload">
-		<div id="page-wrapper">
 
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
 
+    <?php echo auto_discovery_link_tags(); ?>
 
-      <?php
-      if (isset($title)) {
-          $titleParts[] = strip_formatting($title);
-      }
-      $titleParts[] = option('site_title');
-      ?>
-      <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
 
-      <?php echo auto_discovery_link_tags(); ?>
+    <!-- CSS -->
+    <?php
+        queue_css_file(array( 'style'));
+        echo head_css();
+    ?>
 
-      <?php
-      $title = (isset($title)) ? $title : null;
-      $item = (isset($item)) ? $item : null;
-      $tour = (isset($tour)) ? $tour : null;
-      $file = (isset($file)) ? $file : null;
-      ?>
+    <!-- JavaScripts -->
+    <?php
+        queue_js_file(array( 'jquery.min','jquery.dropotron.min', 'jquery.scrollex.min', 'browser.min', 'breakpoints.min', 'util', 'main'));
+        echo head_js();
+    ?>
+</head>
 
-			<!-- Header -->
-				<header id="header" class="alt">
-					<h1><a href="index.php">Mapping Paris</a></h1>
-					<nav id="nav">
-						<ul>
-							<li><a href="index.php">Home</a></li>
-							<li>
-								<a href="#" class="icon fa-angle-down">Navigation Dropdown</a>
-								<ul>
-									<li><a href="generic.php">Generic</a></li>
-									<li><a href="contact.php">Contact</a></li>
-									<li><a href="elements.php">Elements</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option One</a></li>
-											<li><a href="#">Option Two</a></li>
-											<li><a href="#">Option Three</a></li>
-											<li><a href="#">Option Four</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li><a href="#" class="button">Sign In</a></li>
-						</ul>
-					</nav>
-				</header>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
 
-		</div>
+    <!-- Header -->
+    <header id="header" class="alt">
+        <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
+        <h1> <?php echo link_to_home_page(option('site_title')); ?> </h1>
 
-		<!-- Scripts -->
-			<script src="themes/mapping-paris/js/jquery.min.js"></script>
-			<script src="themes/mapping-paris/js/jquery.dropotron.min.js"></script>
-			<script src="themes/mapping-paris/js/jquery.scrollex.min.js"></script>
-			<script src="themes/mapping-paris/js/browser.min.js"></script>
-			<script src="themes/mapping-paris/js/breakpoints.min.js"></script>
-			<script src="themes/mapping-paris/js/util.js"></script>
-			<script src="themes/mapping-paris/js/main.js"></script>
+        <div id="search-container" role="search">
+          <?php echo search_form(array()); ?>
+        </div>
 
-	</body>
-</php>
+        <nav id="nav" role="navigation">
+           <?php echo public_nav_main(); ?>
+        </nav>
+    </header>
