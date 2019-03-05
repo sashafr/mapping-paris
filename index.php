0a8c70ff0bf7c11d484b1f669e40ef28a2bd0d14
Sasha Renninger <sashafr@upenn.edu>
2019-03-05 22:36:46 +0000

    making home page configurable and pulling all public Neatline exhibits

--- index.php
+++ index.php
@@ -1,111 +1,81 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
-<!-- Banner -->
-	<section id="banner">
-		<h2>Alpha</h2>
-		<p>[this is where we will explain what this project is - mapping paris is... (not too sure)].</p>
-		<ul class="actions special">
-			<li><a href="#" class="button primary">Sign Up</a></li>
-			<li><a href="#" class="button">Learn More</a></li>
-		</ul>
-	</section>
-
-<!-- Main -->
-	<section id="main" class="container">
-
-		<section class="box special">
-			<header class="major">
-				<h2>[info]
-				<br />
-				</h2>
-				<p>[subtext]</p>
-			</header>
-			<span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+	<!-- Banner -->
+		<section id="banner" <?php echo 'style="background-image: url('.WEB_ROOT.'/files/theme_uploads/'.get_theme_option('banner_image').')"' ?>>
+			<h2><?php echo option('site_title') ?></h2>
+			<?php if (get_theme_option('Homepage Text')): ?>
+			    <p><?php echo get_theme_option('Homepage Text'); ?></p>
+			<?php endif; ?>
 		</section>
 
-		<section class="box special features">
-			<div class="features-row">
-				<section>
-					<span class="icon major fa-bolt accent2"></span>
-					<p><?php echo recent_items(1); ?></p>
+	<!-- Main -->
+		<section id="main" class="container">
+		    
+            <?php if (plugin_is_active('Neatline')): ?>
+		    
+    			<section class="box special features">
+    		        <?php set_loop_records('NeatlineExhibits', get_records('NeatlineExhibit')); ?>
+    		        <?php $loop_index = 0; ?>
+    		        <?php foreach (loop('NeatlineExhibits') as $e): ?>
+    		            <?php if ($loop_index % 2 == 0): ?><div class="features-row"><?php endif ?>
+        					<section>
+        						<a href="<?php echo nl_getExhibitUrl($e, 'fullscreen') ?>"><h3><?php echo nl_getExhibitField('title') ?></h3></a>
+    					        <p><?php echo snippet_by_word_count(strip_formatting(nl_getExhibitField('narrative')), 50, '...') ?></p>
+    				        </section>
+				        <?php if ($loop_index % 2 == 1): ?></div><?php endif ?>
+				        <?php $loop_index++; ?>
+				    <?php endforeach ?>
+				    <?php if (count(get_records('NeatlineExhibit')) % 2 == 1): ?><section>&nbsp;</section></div><?php endif ?> <!-- have to add a blank section if number of neatline exhibits is odd to make the formatting consistent -->
 				</section>
 
-				<section>
-					<span class="icon major fa-area-chart accent3"></span>
-					<p><?php echo recent_items(1); ?></p>
-				</section>
+            <?php endif; ?>
+
+			<div class="row">
+				<div class="col-6 col-12-narrower">
+
+					<section class="box special">
+						<span class="image featured"><img src="images/pic02.jpg" alt="" /></span>
+						<h3>Sed lorem adipiscing</h3>
+						<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+
+				</div>
+				<div class="col-6 col-12-narrower">
+
+					<section class="box special">
+						<span class="image featured"><img src="images/pic03.jpg" alt="" /></span>
+						<h3>Accumsan integer</h3>
+						<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+
+				</div>
 			</div>
 
-			<div class="features-row">
-				<section>
-					<p><?php echo recent_items(1); ?></p>
-				</section>
-
-				<section>
-					<span class="icon major fa-lock accent5"></span>
-					<p><?php echo recent_items(1); ?></p>
-				</section>
-			</div>
 		</section>
 
-		<div class="row">
-			<div class="col-6 col-12-narrower">
+	<!-- CTA -->
+		<section id="cta">
 
-				<section class="box special">
-					<span class="image featured"><img src="images/pic02.jpg" alt="" /></span>
-				<?php if (get_theme_option('Display Featured Item') !== '0'): ?>
-					<!-- Featured Item -->
-					<div id="featured-item">
-							<h2><?php echo __('Featured Item'); ?></h2>
-							<?php echo random_featured_items(1); ?>
-					</div><!--end featured-item-->
-					<?php endif; ?>
-					<ul class="actions special">
-						<li><a href="#" class="button alt">Learn More</a></li>
-					</ul>
-				</section>
+			<h2>Sign up for beta access</h2>
+			<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
 
-			</div>
-			<div class="col-6 col-12-narrower">
-
-				<section class="box special">
-					<span class="image featured"><img src="images/pic03.jpg" alt="" /></span>
-					<?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
-					<!-- Featured Collection -->
-					<div id="featured-collection">
-							<h2><?php echo __('Featured Collection'); ?></h2>
-							<?php echo random_featured_collection(1); ?>
-					</div><!--end featured-collection-->
-					<?php endif; ?>
-					<ul class="actions special">
-						<li><a href="#" class="button alt">Learn More</a></li>
-					</ul>
-				</section>
-
-			</div>
-			<a href="items/browse" class="button">View All Items</a>
-		</div>
-
-	</section>
-
-<!-- CTA -->
-	<section id="cta">
-
-		<h2>What are you looking for?</h2>
-		<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
-
-		<form>
-			<div class="row gtr-50 gtr-uniform">
-				<div class="col-8 col-12-mobilep">
-					<input type="email" name="email" id="email" placeholder="Email Address" />
+			<form>
+				<div class="row gtr-50 gtr-uniform">
+					<div class="col-8 col-12-mobilep">
+						<input type="email" name="email" id="email" placeholder="Email Address" />
+					</div>
+					<div class="col-4 col-12-mobilep">
+						<input type="submit" value="Sign Up" class="fit" />
+					</div>
 				</div>
-				<div class="col-4 col-12-mobilep">
-					<input type="submit" value="Sign Up" class="fit" />
-				</div>
-			</div>
-		</form>
+			</form>
 
-	</section>
-
-
+		</section>
+		
 <?php echo foot(); ?>
